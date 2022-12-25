f1a6d69307cd194cf0b4afc654e2a4940e1e993a
Pocheng <cpocheng32@gmail.com>
2022-12-25 10:30:59 +0800

    修改加入我的收藏寫法(1)寫入方式改為ajax(2)增加判斷餐廳是否已經加入收藏

--- view/restaurant_detail.php
+++ view/restaurant_detail.php
@@ -8,10 +8,8 @@ $row_detail->bindParam(':rID',$rID,PDO::PARAM_STR);
 $row_detail->execute();
 foreach($row_detail as $k=>$v){
 ?>
-
-<form action="../contral/insert_info.php" method="post">
   <h3><?=$v['name']?></h3>
-  <table width="100%" border="1" style="border-collapse: collapse;">
+  <table width="100%" border="1px" style="border-collapse: collapse;">
     <tr height="50px">
       <td>地區</td>
       <td><?=$v['area']?></td>
@@ -30,7 +28,7 @@ foreach($row_detail as $k=>$v){
     </tr>
     <tr height="50px">
       <td>備註</td>
-      <td colspan="5"><?=$v['memo']?></td>
+      <td colspan="5"><?=nl2br($v['memo'])?></td>
     </tr>
     <tr height="50px">
       <td>午餐價格</td>
@@ -48,21 +46,80 @@ foreach($row_detail as $k=>$v){
       </td>
     </tr>
   </table>
-  <input type="hidden" name="formID" value="add_mylist">
-  <input type="hidden" name="name" value="<?=$v['name']?>">
-  <input type="hidden" name="area" value="<?=$v['area']?>">
-  <input type="hidden" name="location" value="<?=$v['location']?>">
-  <input type="hidden" name="category" value="<?=$v['category']?>">
-  <input type="hidden" name="open_time" value="<?=$v['open_time']?>">
-  <input type="hidden" name="close_time" value="<?=$v['close_time']?>">
-  <input type="hidden" name="access" value="<?=$v['access']?>">
-  <input type="hidden" name="memo" value="<?=$v['memo']?>">
-  <input type="hidden" name="price_lunch" value="<?=$v['price_lunch']?>">
-  <input type="hidden" name="price_dinner" value="<?=$v['price_dinner']?>">
-  <input type="hidden" name="link" value="<?=$v['link']?>">
-  <input type="hidden" name="rID" value="<?=$v['id']?>">
+  <?php
 
-  <input type="submit" value="加入我的收藏">
-</form>
-<?php } ?>
+  $row_check=$dbpdo->prepare("SELECT `rID` FROM `my_restaurant_list` WHERE `rID` = :rID");
+  $row_check->bindParam(':rID',$rID,PDO::PARAM_STR);
+  $row_check->execute();
+  //取得my_restaurant_list裡面的rID判斷是否已經存在
+  $check_rID = $row_check->fetch(PDO::FETCH_ASSOC);
+  
+  if(isset($check_rID['rID'])){
+  ?>
+    <input type="button" id="check_mylist" name="check_mylist" value="已收藏">
+  <?php }else{ ?>
+    <input type="button" id="add_mylist_<?=$v['id']?>" name="add_mylist" value="加入我的收藏" onclick="addto_mylist(this.id);">
+  <?php } ?>
+  <!-- 下方ajax post用的變數 -->
+  <input type="hidden" id="rID" name="rID" value="<?=$v['id']?>">
+  <input type="hidden" id="name" name="name" value="<?=$v['name']?>">
+  <input type="hidden" id="area" name="area" value="<?=$v['area']?>">
+  <input type="hidden" id="location" name="location" value="<?=$v['location']?>">
+  <input type="hidden" id="category" name="category" value="<?=$v['category']?>">
+  <input type="hidden" id="open_time" name="open_time" value="<?=$v['open_time']?>">
+  <input type="hidden" id="close_time" name="close_time" value="<?=$v['close_time']?>">
+  <input type="hidden" id="access" name="access" value="<?=$v['access']?>">
+  <input type="hidden" id="price_lunch" name="price_lunch" value="<?=$v['price_lunch']?>">
+  <input type="hidden" id="price_dinner" name="price_dinner" value="<?=$v['price_dinner']?>">
+  <input type="hidden" id="link" name="link" value="<?=$v['link']?>">
+  <input type="hidden" id="memo" name="memo" value="<?=$v['memo']?>">
+<?php
+}   
+?>
+<br>
 <a type="button" href="../index.php">回首頁</a>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script>
+  function addto_mylist(id) {
+    let rID = $('#rID').val();
+    let name = $('#name').val();
+    let area = $('#area').val();
+    let location = $('#location').val();
+    let category = $('#category').val();
+    let open_time = $('#open_time').val();
+    let close_time = $('#close_time').val();
+    let access = $('#access').val();
+    let memo = $('#memo').val();
+    let price_lunch = $('#price_lunch').val();
+    let price_dinner = $('#price_dinner').val();
+    let link = $('#link').val();
+
+    if (confirm('確認加入我的收藏？')) {
+      $.ajax({
+        url: "../contral/insert_into.php",
+        type: "POST",
+        data: {
+              "formID": "add_mylist",
+              "rID": rID,
+              "name": name,
+              "area": area,
+              "location": location,
+              "category": category,
+              "open_time": open_time,
+              "close_time": close_time,
+              "access": access,
+              "memo": memo, //memo不能直接存$v['memo']不然會報錯
+              "price_lunch": price_lunch,
+              "price_dinner": price_dinner,
+              "link": link
+              },
+          success: function(res) {
+            confirm("已經成功加入我的收藏");
+            document.location.reload(true);
+          }
+      });
+    } else {
+      alert('已取消加入我的收藏');
+    }
+  };
+</script>
